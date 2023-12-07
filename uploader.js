1e0edb091849ac7d9ebb4610cb43cdce1995f36f
Space Kraft <52561908+spacekraftofficial@users.noreply.github.com>
2023-12-07 01:04:53 -0800

    uploadersmpls
    lol god damn im a pissed xD

---
+++ uploader.js
@@ -0,0 +1,21 @@
+/*function createButton() {
+  const button = document.createElement("button");
+  button.textContent = "Unload in Me!!";
+  button.onclick = function () {
+    alert("You Unloaded inside me!");
+  };
+  document.body.appendChild(button);
+}
+
+createButton();*/
+
+/*const button = document.createElement('file-fetch');
+button.textContent = 'Click Me!';
+button.addEventListener('click', () => {
+  alert('Button clicked!');
+});
+
+const body = document.body;
+body.appendChild(button);
+*/
+
